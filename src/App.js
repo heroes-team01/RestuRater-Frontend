ce13efd1abb54b7554c03e354d87156ab91d48d5
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-08-15 12:42:17 +0300

    import dashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
-import Aboutus from './componants/Aboutus';
+import Aboutus from './componants/Aboutus/Aboutus';
+import Indexview from './componants/dashboard/Indexview';
 import { BrowserRouter as Route } from 'react-router-dom';
 
 function App() {
   return (
-<div>
-<Route path='/Aboutus'>
-<Aboutus />
-            </Route>
+
+    <div>
+      <Route path='/Aboutus'>
+        <Aboutus />
+      </Route>
+      <Route path='/admin'>
+      <Indexview />
+      </Route>
+      
+    </div>
   
-</div>
   );
 }
 
