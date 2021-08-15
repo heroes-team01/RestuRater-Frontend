9401148fc96eebc33f181ab91ec6e3f6095d0841
Amneh-momani <Amneh99el@gmail.com>
2021-08-15 09:53:43 -0700

    ..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+import { BrowserRouter as Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import React from "react";
-import Category from "./components/category";
+import Main from "./components/main";
+import restuData from "./components/resturater.json";
+
 class App extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: restuData,
+    };
+  }
+
   render() {
     return (
-      <Container align="center">
-
-        <Category />
-   </Container>
+      <div>
+        <Main restData={this.state.data} />
+      </div>
     );
   }
 }
-
 export default App;
