4c82196143e51e9eadbe2de23989032141176132
Amneh-momani <Amneh99el@gmail.com>
2021-08-15 18:51:33 -0700

    ..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
-
-import { BrowserRouter as Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
 import React from "react";
 import Main from "./components/main";
 import restuData from "./components/resturater.json";
