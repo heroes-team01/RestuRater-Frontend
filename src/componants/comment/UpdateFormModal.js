51e2440c110ccdf74089eb55f00c168d9b786a48
Heba1998 <hebaalmomani1998@gmail.com>
2021-08-17 21:59:21 +0300

    comment

--- src/componants/comment/UpdateFormModal.js
+++ src/componants/comment/UpdateFormModal.js
@@ -8,50 +8,29 @@ export class UpdateFormModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      catName: this.props.catObject.cat_name,
-      catBreed: this.props.catObject.cat_breed,
-      catImage: this.props.catObject.cat_img,
-      catId: this.props.catObject._id,
+      name: this.props.comObject.rest_name,
+      comment: this.props.comObject.rating_comment,
+      image: this.props.comObject.user_img,
+      id: this.props.idx,
     }
   }
 
-  handelNameChange = (e) => this.setState({ catName: e.target.value });
-  handelBreedChange = (e) => this.setState({ catBreed: e.target.value });
-  handelImageChange = (e) => this.setState({ catImage: e.target.value });
+  handelNameChange = (e) => this.setState({ name: e.target.value });
+  handelBreedChange = (e) => this.setState({ comment: e.target.value });
+  handelImageChange = (e) => this.setState({ image: e.target.value });
 
   handelSubmitForm = ((e) => {
     e.preventDefault();
-    const catId = this.state.catId;
+    // const id = this.state.id;
     const body = {
-      cat_name: this.state.catName,
-      cat_breed: this.state.catBreed,
-      cat_img: this.state.catImage,
+      rest_name: e.target.name.value,
+      rating_comment: e.target.comment.value,
+      user_img: e.target.image.value,
     };
-
-    axios.put(`${process.env.REACT_APP_SERVER}/cat/${catId}`, body).then((axiosResponse) => {
-      console.log('updated Cat Data:  ', axiosResponse.data);
-
-      // once we get the new updated data from axios from the backend, we need to reflect it on the frontend without refreshing
-
-      // we will use the map function to loop through the array of cat that we have. find the the cat that got updated by its ID and 
-      // update the data for that cat
-
-      const updatedCatArr = this.props.catsArr.map(cat => {
-
-        if (cat._id === catId) {
-          cat.cat_name = axiosResponse.data.cat_name;
-          cat.cat_breed = axiosResponse.data.cat_breed;
-          cat.cat_img = axiosResponse.data.cat_img;
-
-          return cat;
-        }
-        return cat;
-
-      });
-      this.props.updateCats(updatedCatArr);
-
-
-
+  
+    axios.put(`${process.env.REACT_APP_SERVER}/review/${this.state.id}`, body).then((axiosResponse) => {
+      console.log( axiosResponse.data);
+     
       this.props.handelDisplayModal({}); // to  close the modal and reset the object of the cat to be updated
     }).catch(error => alert(error))
   });
@@ -61,24 +40,24 @@ export class UpdateFormModal extends Component {
       <div>
         <Modal show={this.props.show} onHide={this.props.handelDisplayModal}>
           <Modal.Header>
-            <Modal.Title>Update Cat</Modal.Title>
+            <Modal.Title>Update comment</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Form onSubmit={(e) => this.handelSubmitForm(e)}>
               <Form.Group className="mb-3">
-                <Form.Label>Cat Name</Form.Label>
-                <Form.Control onChange={(e) => this.handelNameChange(e)} value={this.state.catName} type="text" placeholder="Enter the cats name" />
+                <Form.Label>Resturant Name</Form.Label>
+                <Form.Control onChange={(e) => this.handelNameChange(e)} value={this.state.name} type="text" name='name' />
               </Form.Group>
               <Form.Group className="mb-3">
-                <Form.Label>Cat Breed</Form.Label>
-                <Form.Control onChange={(e) => this.handelBreedChange(e)} value={this.state.catBreed} type="text" placeholder="Enter the cats breed" />
+                <Form.Label>comment</Form.Label>
+                <Form.Control onChange={(e) => this.handelBreedChange(e)} value={this.state.comment} type="text" name='comment' />
               </Form.Group>
               <Form.Group className="mb-3">
-                <Form.Label>Cat Image</Form.Label>
-                <Form.Control onChange={(e) => this.handelImageChange(e)} value={this.state.catImage} type="text" placeholder="Enter the image URL" />
+                <Form.Label>Image</Form.Label>
+                <Form.Control onChange={(e) => this.handelImageChange(e)} value={this.state.image} type="text" placeholder="Enter the image URL"  name='image' />
               </Form.Group>
               <Button variant="primary" type="submit">
-                Update Cat
+                Update comment
               </Button>
             </Form>
           </Modal.Body>
